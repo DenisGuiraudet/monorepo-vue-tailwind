010bf355e827bdac59abe0d30017edd40f68dc9c
DenisGuiraudet <denis@guidap.co>
2023-06-30 11:51:52 +0200

    add storybook

---
+++ apps/storybook/stories/HelloWorld.stories.ts
@@ -0,0 +1,13 @@
+import { HelloWorld } from '@guidap/ui'
+
+export default {
+  title: 'Ui/HelloWorld',
+  component: HelloWorld,
+  tags: ['autodocs']
+};
+
+export const Default = {
+  args: {
+    msg: 'coucou Toto'
+  },
+};
